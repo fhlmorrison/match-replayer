6cc8e2249b21e1b76e7d2488c8700c1cd86111de
Fraser Morrison <frazemor@hotmail.com>
2023-03-29 03:57:40 -0400

    Added playback bar to app

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+  import PlaybackBar from "./lib/PlaybackBar.svelte";
   import Video from "./lib/Video.svelte";
 </script>
 
 <main class="container">
   <div class="row">
     <Video />
+  </div>
+  <div class="row">
+    <PlaybackBar />
   </div>
 </main>
