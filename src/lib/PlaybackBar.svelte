9eaa899709e332f2695cb6b61a7a120ebfcee9ef
Fraser Morrison <frazemor@hotmail.com>
2023-03-29 05:45:47 -0400

    Removed old slider

--- src/lib/PlaybackBar.svelte
+++ src/lib/PlaybackBar.svelte
@@ -56,28 +56,9 @@
     step={1 / (10 * FRAMERATE)}
     style={`--input-value: ${($currentTime * 100) / $totalTime}%`}
   />
-  <!-- Playback slider -->
-  <div class="playback-bar">
-    <div
-      class="playback-bar__slider"
-      on:click={jumpTo}
-      bind:this={slider}
-      on:keydown={keySeek}
-    >
-      <div
-        class="playback-bar__slider__progress"
-        style={`width: ${($currentTime * 100) / $totalTime}%`}
-      />
-    </div>
-    <div class="playback-bar__time">
-      <span class="playback-bar__time__current">{timeFormat($currentTime)}</span
-      >
-      <span>/</span>
-      <span class="playback-bar__time__total">{timeFormat($totalTime)}</span>
-    </div>
-  </div>
   <!-- Pause button -->
   <div class="button-bar">
+    <div />
     <div class="button-bar_group">
       <button class="icon" on:click={seek(-5)}>
         <MdFastRewind />
@@ -93,13 +74,16 @@
         <MdFastForward />
       </button>
     </div>
+    <div class="time">
+      {timeFormat($currentTime)} / {timeFormat($totalTime)}
+    </div>
   </div>
 </div>
 
 <!-- markup (zero or more items) goes here -->
 <style>
   :root {
-    --slider-primary-color: #f00;
+    --slider-primary-color: #bd0909;
     --slider-secondary-color: #000;
     --light-text-color: #fff;
   }
@@ -108,6 +92,7 @@
     -webkit-appearance: none;
     appearance: none;
     background: transparent;
+    box-shadow: none;
     cursor: pointer;
     width: 100%;
     height: 10px;
@@ -156,9 +141,8 @@
   }
 
   .button-bar {
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
+    display: inline-grid;
+    grid-template-columns: 1fr 2fr 1fr;
     width: 100%;
     height: 40px;
     border-radius: 10px;
@@ -167,67 +151,14 @@
   .button-bar_group {
     display: flex;
     align-items: center;
-    justify-content: space-evenly;
-    width: 25%;
+    justify-content: center;
+    gap: 10px;
     height: 100%;
   }
 
-  .playback-bar {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    background-color: #f00;
-    width: 100%;
-    height: 100%;
-    border-radius: 10px;
-  }
-
-  .playback-bar__slider {
-    position: relative;
-    width: 100%;
-    height: 10px;
-    background-color: #fff;
-    border-radius: 5px;
-    margin: 0 10px;
-  }
-
-  .playback-bar__slider__progress {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 0;
-    height: 100%;
-    background-color: #000;
-    border-radius: 5px;
-  }
-  /* 
-  .playback-bar__slider__progress::after {
-    content: "";
-    position: absolute;
-    top: -5px;
-    right: -5px;
-    width: 20px;
-    height: 20px;
-    background-color: #000;
-    border-radius: 50%;
-  } */
-
-  .playback-bar__time {
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    width: 100px;
-    height: 100%;
-    color: #fff;
-  }
-
-  .playback-bar__time__current {
-    font-size: 0.8rem;
-    color: #fff;
-  }
-
-  .playback-bar__time__total {
-    font-size: 0.8rem;
-    color: #fff;
+  .time {
+    /* color: #000; */
+    font-size: 1.2rem;
+    justify-self: end;
   }
 </style>
