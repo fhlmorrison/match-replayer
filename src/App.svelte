e76c51d0ce6669c866508701e5eb6a90a19cabf2
Fraser Morrison <frazemor@hotmail.com>
2023-04-01 02:59:47 -0400

    Removed unuse dimport

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import GridContainer from "./lib/GridContainer.svelte";
   import PlaybackBar from "./lib/PlaybackBar.svelte";
-  import Video from "./lib/Video.svelte";
 </script>
 
 <main class="container">
